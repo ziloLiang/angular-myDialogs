9d96dedc57739c68b37008013be301166f29b46e
ziloLiang <liangdong891228@126.com>
2015-06-26 12:20:36 +0800

    version-1.0.1
    添加myFream 模板示例  通过grunt构建

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,13 +7,24 @@ module.exports = function (grunt) {
         banner: '/*! <%= pkg.file %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'js/<%=pkg.file %>.js',
-        dest: 'js/<%= pkg.file %>.min.js'
+        files:{
+          'dest/js/<%=pkg.file %>.min.js' : ['src/js/<%=pkg.file %>.js']
+        }
+      }
+    },
+    cssmin: {
+      compress: {
+        files: {
+          'dest/css/myDialogs.min.css': ["src/css/myDialogs.css"]
+        }
       }
     }
   });
   // 加载提供"uglify"任务的插件
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  
+  
   // 默认任务
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify','cssmin']);
 }
